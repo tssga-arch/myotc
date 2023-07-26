83b4c22a7b859b431d83768afb6a3ea3e0b78d4f
Alejandro Liu <AlejandroL@t-systems.com>
2023-07-27 00:39:10 +0200

    WIP: winbuild

--- scripts/gitver.py
+++ scripts/gitver.py
@@ -3,21 +3,24 @@
 #
 import subprocess
 import sys
+import os
 
-cwd = '.'
-rc = subprocess.run(['git','describe'],
+version = os.getenv('GITHUB_REF_NAME')
+if version is None:
+  cwd = '.'
+  rc = subprocess.run(['git','describe'],
                       capture_output=True,
                       text=True,
                       cwd=cwd)
-if rc.returncode == 0:
-  outp = 'VERSION = \'{}\'\n'.format(rc.stdout.strip())
-  if len(sys.argv) == 2:
-    with open(sys.argv[1],'w') as fp:
-      fp.write(outp)
-  else:
-    print(outp)
-else:
-  sys.stderr.write('RUN ERROR: {}\n'.format(str(rc)))
-  sys.exit(1)
+  if rc.returncode != 0:
+    sys.stderr.write('RUN ERROR: {}\n'.format(str(rc)))
+    sys.exit(1)
+  version = rc.stdout.strip()
 
-  
+
+outp = 'VERSION = \'{}\'\n'.format(version)
+if len(sys.argv) == 2:
+  with open(sys.argv[1],'w') as fp:
+    fp.write(outp)
+else:
+  print(outp)
